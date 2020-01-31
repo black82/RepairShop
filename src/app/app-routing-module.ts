2bf22b3d84b9d98b0752a1d476581feee7b7e4a5
Iurie Railean <blackrailean@gmail.com>
2020-01-31 20:16:56 +0100

    develop output device component

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './component/home/home.component';
 import {DeviceinputComponent} from './component/deviceinput/deviceinput.component';
+import {OtpoutDeviceComponent} from './component/otpout-device/otpout-device.component';
 
 
 export const routers: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'enter', component: DeviceinputComponent}
+  {path: '', component: HomeComponent},
+  {path: 'enter', component: DeviceinputComponent},
+  {path: 'return', component: OtpoutDeviceComponent}
 ];
 
 @NgModule({
