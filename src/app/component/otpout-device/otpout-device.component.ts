7979ee94ba77e64a4fd8ee9d25efdc630ce07d0b
Iurie Railean <blackrailean@gmail.com>
2020-01-31 19:03:36 +0100

    develop output device component

---
+++ src/app/component/otpout-device/otpout-device.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-otpout-device',
+  templateUrl: './otpout-device.component.html',
+  styleUrls: ['./otpout-device.component.css']
+})
+export class OtpoutDeviceComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
