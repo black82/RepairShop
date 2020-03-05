f9390608b1606e1230fa4dd35ae887404d53a557
Iurie Railean <blackrailean@gmail.com>
2020-03-05 01:02:30 +0100

    Alert refactoring

--- src/app/component/alert/alert.component.ts
+++ src/app/component/alert/alert.component.ts
@@ -6,13 +6,18 @@ import {Alert, AlertType} from '../entity/AlertEntity';
 import {Router} from '@angular/router';
 import {faCheckCircle} from '@fortawesome/free-solid-svg-icons/faCheckCircle';
 import {faBomb} from '@fortawesome/free-solid-svg-icons/faBomb';
-import {faRadiation} from '@fortawesome/free-solid-svg-icons/faRadiation';
+
 import {faInfoCircle} from '@fortawesome/free-solid-svg-icons/faInfoCircle';
+
+import {faExclamationCircle} from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
+import {faTimesCircle} from '@fortawesome/free-solid-svg-icons';
+import {faCheckSquare} from '@fortawesome/free-solid-svg-icons/faCheckSquare';
+
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css']
+  styleUrls: ['./alert.component.css'],
 })
 export class AlertComponent implements OnInit {
   private typeAlert: string;
@@ -24,6 +29,8 @@ export class AlertComponent implements OnInit {
   private icon: any;
   private color_icon: any;
   private info = faInfoCircle;
+  private error_status: number;
+  private ok = faCheckSquare;
 
   constructor(private alertService: AlertServiceService, private router: Router) {
 
@@ -82,30 +89,33 @@ export class AlertComponent implements OnInit {
   private text_alert_initialized(): void {
     switch (this.alert.type) {
       case AlertType.Success: {
-        this.color_icon = '#fff';
+        this.color_icon = '#06D85F';
         this.icon = faCheckCircle;
         this.title_alert = ' Is Ok';
         this.body_alert = 'Everything went well. Your operation is successfully completed.';
         break;
       }
       case AlertType.Error: {
-        this.color_icon = 'grey';
-        this.icon = faBomb;
+        this.color_icon = '#F09EA3';
+        this.icon = faTimesCircle;
+
         if (this.alert.errore === null || this.alert.errore === undefined) {
           this.alert.errore = new HttpErrorResponse({error: 'Unknown error', status: 404, statusText: 'an unknown error occurred'});
         }
-        this.title_alert = 'Oops : Error Status   ' + this.alert.errore.status;
+        this.error_status = this.alert.errore.status;
+        this.title_alert = 'Oops : Error ';
         this.body_alert = 'Try again. Otherwise contact support.';
         break;
       }
       case AlertType.Warning: {
-        this.color_icon = 'red';
-        this.icon = faRadiation;
+        this.color_icon = '#FFCC00';
+        this.icon = faExclamationCircle;
         this.title_alert = 'Oops : Waring';
         this.body_alert = 'Attention. Something\'s not right.';
         break;
       }
       default: {
+        this.color_icon = '#F09EA3';
         this.icon = faBomb;
         this.title_alert = 'Oops : Error';
         this.body_alert = 'Try again. Otherwise contact support.';
