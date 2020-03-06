4943306e27235e2f2819568f80d68ed0063a623d
black82 <blackrailean@gmail.com>
2020-03-06 14:45:18 +0100

    Modified home page from media large

--- src/app/component/searchform/searchform.component.ts
+++ src/app/component/searchform/searchform.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {faSearch} from '@fortawesome/free-solid-svg-icons/faSearch';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Client} from '../entity/ClientWeb';
 import {HttpClien} from '../service/clientservice.service';
 import {AlertServiceService} from '../service/alert-service.service';
@@ -23,7 +23,7 @@ export class SearchformComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private client_service: HttpClien, private alert_service: AlertServiceService) {
     this.formInput = this.fb.group({
-      ob: new FormControl('')
+      ob: new FormControl(null, Validators.compose([Validators.nullValidator, Validators.pattern(/^((?!\s{2,}).)*$/)]))
     });
   }
 
@@ -32,12 +32,18 @@ export class SearchformComponent implements OnInit {
 
 
   submitForm() {
+    if (!this.formInput.valid) {
+      this.alert_service.warn('',
+        'Please fill in the search field, The field cannot be empty.', false, false, '');
+      return;
+    }
     this.client_service.searchByTelephoneNumber(this.formInput.controls.ob.value).subscribe(
       client => {
         this.client = client;
         this.actionA.emit(this.client);
       },
       error => {
+        console.log(error);
         this.alert_service.error(null,
           'Unfortunately we could not find this client '
           + this.formInput.controls.ob.value + ' please try with other search data.', false, true, '', error)
